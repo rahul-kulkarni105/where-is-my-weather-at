2f7940837c84f7462cbffa54a80b726527c571a4
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2020-12-29 09:36:11 -0600

    feat: Add oneCall weather api call - Add oneCall to get weekly weather - Create WeeklyWeather Component to display the day, max and min temperature - Create action, reducers and error handling for the oneCall api - Add Progress indicator for API call - Clean up for unwanted code - Add Util function to calculate the current day and days of the week from current day.

---
+++ src/components/WeeklyWeather.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import {
+  getAllWeatherApiStatus,
+  selectAllWeatherInfo,
+  weeklyWeatherInfo
+} from '../redux/weatherSlice'
+
+export const WeeklyWeather = () => {
+  const allWeatherApiStatus = useSelector(getAllWeatherApiStatus)
+  const allWeatherDescription = useSelector(selectAllWeatherInfo)
+  const weeklyWeather = useSelector(weeklyWeatherInfo)
+
+  return (
+    <section>
+        {
+          allWeatherApiStatus === 'succeeded'
+          &&
+          <>
+            <div className="text-capitalize">
+              <span>Timezone: </span>{allWeatherDescription.timezone}
+            </div>
+            <div className="week">
+              {
+                weeklyWeather.map(({
+                  dayOfTheWeek,
+                  maxTemp,
+                  minTemp
+                }) => {
+                  return (
+                    <div className="weekday">
+                      <p className="weekday-value">{dayOfTheWeek}</p>
+                      <p className="weekday-value">{maxTemp}</p>
+                      <p className="weekday-value">{minTemp}</p>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          </>
+        }
+        {
+          allWeatherApiStatus === 'failed'
+          &&
+          <>
+            <span>Sorry, we couldn't load your weather information</span>
+          </>
+        }
+    </section>
+  );
+}
--- src/redux/weatherSlice.js
+++ src/redux/weatherSlice.js
@@ -5,13 +5,18 @@ import {
 } from '@reduxjs/toolkit';
 
 import {
-  WEATHER_API_CONTEXT
+  WEATHER_API_CONTEXT,
+  WEATHER_ONECALL_API_CONTEXT
 } from '../Utils/globalConstants'
+import { getDates } from '../Utils/utils'
 
 const initialState = {
   weather: {},
   status: 'idle',
-  error: null
+  error: null,
+  allWeather: {},
+  allWeatherError: null,
+  allWeatherStatus: 'idle',
 }
 const sliceName = 'weather'
 
@@ -27,6 +32,24 @@ export const getCurrentLocationWeather = createAsyncThunk(
       return response.data
     } catch(err) {
       console.log(err, 'error in open weather api')
+      return err
+    }
+})
+
+// Can make use of this one API call instead of two to get weather information
+// for further refactoring.
+export const getWeatherInOneCall = createAsyncThunk(
+  `${sliceName}/getWeatherInOneCall`,
+  async ({ lat, lon }) => {
+    // baseUrl can also be part of the env config if needed.
+    const baseUrl = `${process.env.REACT_APP_OPEN_WEATHER_MAP_API_BASE_URL}${WEATHER_ONECALL_API_CONTEXT}`
+    const apiKey = process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY;
+    const fullUrl = `${baseUrl}?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly&appid=${apiKey}`
+    try {
+      const response = await requestHandler({}).get(fullUrl)
+      return response.data
+    } catch(err) {
+      console.log(err, 'error in open weather oneCall api')
       return err
     }
 })
@@ -51,6 +74,17 @@ export const weatherSlice = createSlice({
     [getCurrentLocationWeather.rejected]: (state, action) => {
       state.status = 'failed'
       state.error = action.payload
+    },
+    [getWeatherInOneCall.pending]: (state) => {
+      state.allWeatherStatus = 'loading'
+    },
+    [getWeatherInOneCall.fulfilled]: (state, action) => {
+      state.allWeatherStatus = 'succeeded'
+      state.allWeather = action.payload;
+    },
+    [getWeatherInOneCall.rejected]: (state, action) => {
+      state.allWeatherStatus = 'failed'
+      state.allWeatherError = action.payload
     },
   },
 });
@@ -79,3 +113,21 @@ export const getTemperatureInfo = ({
     }
   }
 }) => main
+export const getAllWeatherApiStatus = (state) => state.weather.allWeatherStatus
+export const selectAllWeatherInfo = (state) => state.weather.allWeather
+export const weeklyWeatherInfo = ({
+  weather: {
+    allWeather: {
+      daily = [],
+    }
+  }
+}) => {
+  const daysWeek = getDates()
+  const formatDailyWeather = daily.map(
+    (day, index) => ({
+      dayOfTheWeek: daysWeek[index],
+      maxTemp: day.temp.min,
+      minTemp: day.temp.max
+    }))
+  return formatDailyWeather;
+}
